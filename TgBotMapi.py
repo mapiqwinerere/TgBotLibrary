7fc1049a1d061fb636baccedbf9eab10c0dd2818
mapiqwinerere <toleg9410@gmail.com>
2023-05-02 23:14:19 +0800

    final version

--- TgBotMapi.py
+++ TgBotMapi.py
@@ -12,43 +12,46 @@ def write_token(bot, token):
        file.close()
 
 class Keyboard():
-   def __init__(self, name, row_width, buts, value, text, send, message, bot):
+   def __init__(self, name, row_width, buts, values, text, send, message, bot):
       global buts_value
       global keyboards
+      global buts_value
 
       self.name = name
       self.row_width = row_width
       self.buts = buts
-      self.value = value
+      self.value = values
       self.text = text
-      self.send = send
+      self.send_or_not = send
       self.message = message
       self.keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width= row_width)
       keyboards = {}
+      buts_value = {}
       keyboards[name] = {}
-      
-      for but in buts:
-          keyboards[name]['but' + str(but)] = types.KeyboardButton(but)
-          self.buts.append(but)
-      
-      for i in range(len(self.buts)):
-          self.buts_value[self.buts[i]] = self.value[i]
-   
-      dictionary_values = list(self.keyboards[name_keyboard].values())
-      keyboard.add(*dictionary_values)
 
-      if self.send:
-          self.keyboard.send(bot)
+      for value in values:
+          bot.values.append(value)
+
+      for but in self.buts:
+          bot.buts.append(but)
+
+          keyboards[name]['but_' + str(but)] = types.KeyboardButton(but)
+
+      for i in range(len(self.buts)):
+          buts_value[buts[i]] = self.value[i]
+
+      list_values = list(keyboards[name].values())
+      self.keyboard.add(*list_values)
+
+      if self.send_or_not:
+          bot.bot.send_message(self.message.chat.id, text = self.text.format(self.message.from_user), reply_markup=self.keyboard)
 
    def send(self, bot):
-      bot.bot.send_message(self.message.chat.id, text = self.text_user.format(message.from_user), reply_markup=self.keyboard)
+      bot.bot.send_message(self.message.chat.id, text = self.text.format(self.message.from_user), reply_markup=self.keyboard)
 
 class TgBot():
     def __init__(self, name_bot): 
         global bot
-        global keyboards
-        global buts
-        global buts_value
 
         bots_list = []
         bots_dict = {}
@@ -70,50 +73,23 @@ class TgBot():
                token = bots_dict.get(name_bot)
             elif token == None:
                 print("None")
+
         self.bot = telebot.TeleBot(token)
-       
-        self.keyboards = {}
         self.buts = []
         self.buts_value = {}
 
-    #function to launch telegram bot
+
     def start(self):
        @self.bot.message_handler(content_types='text')
        def check_2(message):
-            for but in self.buts:
-               if(message.text == but):
-                  self.buts_value.get(but)(message)
+          for but in self.buts:
+             if(message.text == but):
+                buts_value.get(but)(message)
        self.bot.polling()
     
     #function to send messages
     def message(self, text, message):
         self.bot.send_message(message.chat.id, text)
-
-    #function to create keyboards    
-    def keyboard(self, buts, values, name_keyboard, row_width, text_user, send, message):
-       global buts_value
-
-       keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width= row_width)
-       self.keyboards[name_keyboard] = {}
-       
-       #add buttons
-       for but in buts:
-           self.keyboards[name_keyboard]['but' + str(but)] = types.KeyboardButton(but)
-           self.buts.append(but)
-       
-       #assignment function for buttons
-       for i in range(len(buts)):
-           self.buts_value[buts[i]] = values[i]
-           self.values = values
-      
-       dictionary_values = list(self.keyboards[name_keyboard].values())
-       keyboard.add(*dictionary_values)
-       
-       #send keyboard if send = True
-       if send:
-           self.bot.send_message(message.chat.id, text = text_user.format(message.from_user), reply_markup=keyboard)
-    
-    #function to create hadler
  
     def keyboard(self, name_keyboard, row_width, buts, value, text, send, message):
         self.keyboards[name_keyboard] = Keyboard(name_keyboard, row_width, buts, value, text, send, message)
